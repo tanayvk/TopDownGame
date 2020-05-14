f5d70810c7d8be9b7f7fa25cbf19ac7503723e39
Tanay Karnik <tanayvk@gmail.com>
2020-05-14 18:54:52 +0530

    Attempt fix for windows-build in CI

--- CMake/SDL2Config.cmake
+++ CMake/SDL2Config.cmake
@@ -1,10 +1,10 @@
 # sdl2 cmake project-config input for ./configure scripts
 
-set(prefix "${CMAKE_CURRENT_LIST_DIR}/i686-w64-mingw32") 
+set(prefix "${CMAKE_CURRENT_LIST_DIR}/x86_64-w64-mingw32") 
 set(exec_prefix "${prefix}")
 set(libdir "${exec_prefix}/lib")
-set(SDL2_PREFIX "${CMAKE_CURRENT_LIST_DIR}/i686-w64-mingw32")
-set(SDL2_EXEC_PREFIX "${CMAKE_CURRENT_LIST_DIR}/i686-w64-mingw32")
+set(SDL2_PREFIX "${CMAKE_CURRENT_LIST_DIR}/x86_64-w64-mingw32")
+set(SDL2_EXEC_PREFIX "${CMAKE_CURRENT_LIST_DIR}/x86_64-w64-mingw32")
 set(SDL2_LIBDIR "${exec_prefix}/lib")
 set(SDL2_INCLUDE_DIRS "${prefix}/include/SDL2")
 set(SDL2_LIBRARIES "-L${SDL2_LIBDIR}  -lmingw32 -lSDL2main -lSDL2 -mwindows")
